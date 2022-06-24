0bba971aa11a21b4a71bc25a8225a1429e329795
gorororo <gorororo87@gmail.com>
2022-06-24 17:51:59 +0900

    220624수업끝

---
+++ training/Exercise_file/circleModule.py
@@ -0,0 +1,10 @@
+# circleModule.py
+PI = 3.14
+
+def getCircumference(radius):
+    return 2*PI*radius
+
+def getCircleArea(radius):
+    return PI*radius*radius
+
+print('circleModule', PI)
---
+++ training/circleModule.py
@@ -0,0 +1,13 @@
+# circleModule.py
+PI = 3.14
+
+def getCircumference(radius):
+    return 2*PI*radius
+
+def getCircleArea(radius):
+    return PI*radius*radius
+
+# print('circleModule', PI)
+
+if __name__=="__main__":
+    print('main_circleModule', PI)
