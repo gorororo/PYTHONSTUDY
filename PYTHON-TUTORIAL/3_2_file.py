7a0f9c4417dafb4140a8a1d63e9ce9d84f01b391
gorororo <gorororo87@gmail.com>
2022-06-28 01:07:00 +0900

    home_study

---
+++ PYTHON-TUTORIAL/3_2_file.py
@@ -0,0 +1,4 @@
+print(1+1)
+a=2
+b=2
+print(a+b)
---
+++ PYTHON-TUTORIAL/4_1_number.py
@@ -0,0 +1,17 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+print('1+1',1+1) 
+print('2-1',2-1)
+print('2*2',2*2)
+print('4/2',4/2)
+
+import math as ma
+
+print ('ma.sqrt(4)',ma.sqrt(4)) #제곱근
+print ('ma.pow(4,2)',ma.pow(4,2)) #제곱
+
+import random 
+
+print('random.random()',random.random())
