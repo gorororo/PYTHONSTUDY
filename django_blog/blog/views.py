453e6adf067b37047e4394a61decedd2288a8471
gorororo <gorororo87@gmail.com>
2022-07-18 15:41:27 +0900

    장고로 블로그 만들어보기

---
+++ django_blog/blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def post_list(request):
+  posts = Post.objects.all()
+  return render(
+    request, 
+    'blog/post_list.html',
+    {'pposts':posts}
+    )
+
+def post_detail(request, pk):
+  post = Post.objects.get(pk=pk)
+  return render(
+    request, 
+    'blog/post_detail.html',
+    {'ppost':post}
+    )
